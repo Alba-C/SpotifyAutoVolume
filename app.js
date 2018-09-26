f1cabc3ecb3489dc35df518fe4f3cde25ea79fea
REAOrlando <Chris@REAOrlando.com>
2018-09-26 12:26:10 -0400

    test new files actually contain JSON data instead of an img file

--- app.js
+++ app.js
@@ -25,44 +25,56 @@ fs.watch(watchPath, (change, filename) => {
 
     let currentVolume;
     fs.readFile(watchPath + filename, "utf8", (err, data) => {
-      if (err) console.log(err);
-      let json = JSON.parse(data);
+      if (err) console.log("Error reading file: ", err);
 
-      // Not all files contain ads that will play, so we only need to track the ones that contain a "stream" array.
-      if (json.pod.stream) {
-        let totalReduce = json.pod.stream
-          // some are marked dummy: true, these don't play so you can ignore the "duration_sec" for these
+      //make sure the file contains JSON, some are image files
+      if (isJsonString(data)) {
+        let json = JSON.parse(data);
+        // Not all files contain ads that will play, so we only need to track the ones that contain a "stream" array.
+        if (json.pod.stream) {
+          let totalReduce = json.pod.stream
+            // some are marked dummy: true, these don't play so you can ignore the "duration_sec" for these
 
-          .filter(({ dummy }) => !dummy)
-          .reduce((a, b) => {
-            if (b.video) {
-              return a + b.video[0].duration_sec;
-            }
+            .filter(({ dummy }) => !dummy)
+            .reduce((a, b) => {
+              if (b.video) {
+                return a + b.video[0].duration_sec;
+              }
 
-            if (b.audio) {
-              return a + b.audio[0].duration_sec;
-            }
-          }, 2);
+              if (b.audio) {
+                return a + b.audio[0].duration_sec;
+              }
+            }, 2);
 
-        adPlaying
-          ? (duration += totalReduce * 1000)
-          : (duration = totalReduce * 1000);
-        adPlaying = true;
-        console.log("totalReduce = ", totalReduce, "duration = ", duration);
+          adPlaying
+            ? (duration += totalReduce * 1000)
+            : (duration = totalReduce * 1000);
+          adPlaying = true;
+          console.log("totalReduce = ", totalReduce, "duration = ", duration);
 
-        system.audio.getSystemVolume().then(volume => {
-          currentVolume = volume;
-          system.audio.setSystemVolume(minVolume).then(() => {
-            console.log("volume set to : ", minVolume);
-            setTimeout(() => {
-              system.audio.setSystemVolume(currentVolume).then(() => {
-                console.log("volume returned to : ", currentVolume);
-                adPlaying = false;
-              });
-            }, duration);
+          system.audio.getSystemVolume().then(volume => {
+            currentVolume = volume;
+            system.audio.setSystemVolume(minVolume).then(() => {
+              console.log("volume set to : ", minVolume);
+              setTimeout(() => {
+                system.audio.setSystemVolume(currentVolume).then(() => {
+                  console.log("volume returned to : ", currentVolume);
+                  adPlaying = false;
+                });
+              }, duration);
+            });
           });
-        });
+        }
       }
     });
   }
 });
+
+const isJsonString = str => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
